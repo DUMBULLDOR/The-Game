7d484975a456e643855c562abe680d2c64818af3
Atomsmasher <30929354+DUMBULLDOR@users.noreply.github.com>
2017-12-09 18:44:18 +0100

    Create Welcomefile.py

---
+++ Welcomefile.py
@@ -0,0 +1,77 @@
+me = "Amen"
+def run():
+  print(" You choose to run and you run down a corridor.\n There are two doors\n")
+  print("First door \n There is A lion in there, that lion hasn't eated in years\n")
+  print("Second door \n There is a seriel killer from the 90's\n")
+  a = input("What door do you choose \n (1(first door) or 2(second door)\n")
+  if a == 1:
+    print("Huh looks like you've choosen the right the door,\n cause that lion looks dead")
+    print("Oh My God  is that you?\n")
+    print("Yea it's me who are you?\n")
+    print("Don't you remember me? it's me\n")
+    print("But you look different\n")
+    print("Yea no sh*t")
+  elif a is not 1 or 2:
+    print("There is no door like that,\n  There is only door 1 and 2 :D")
+  else:
+    print("So you've chosen the door with a serial killer from the 90's not so good cause now you are dead :D")
+print("")
+bullets = random.randint(2,10)
+print("Hi my friend")
+print("hmmm.. don't like the sound of that...")
+print("Gotta call you something else...")
+name = input("What can i call you? :")
+print("")
+print("")
+print("Hello "+ str(name) +" i like that name way better than freinds but hey we can be friends :D ")
+print("")
+print("")
+print("Hi my name is "+ me +" if you are wondering iam a girl (just to make this game non-sexsist)")
+input("Press anything to continue")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print(me +": huh?")
+print("")
+print("1 = asnwer What?")
+print("2 = be quiet ")
+print("")
+print("Choose what to do 1 or 2 ?")
+input("-->")
+print(me +": I think i heard something ")
+print(me +": SWEET LORD WHAT THE F*CK IS THAT ")
+print(me +": AHHHHHHHHHHHHH RUN "+ name +" RUN!!!!!!")
+print("1 = run")
+print("2 = Try to help her")
+print("3 = kill yourself ")
+a = input("What do you do? asnwer 1,2 or 3 ")
+if a == 1:
+  pass
+  run()
+elif a == 2:
+  pass
+  hero()
+elif a == 3:
+  pass
+  suicide()
+elif a is not 1 or 2 or 3:
+  print("You didn't choose 1 or 2 nor did you choose 3 don't choose 3  try to run the game again")
+
+
+
+
+
